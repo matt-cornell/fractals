bedd38afef8bd1ab3ec1400e91672554e49e3d13
Matthew Cornell <mcornell400708@gmail.com>
2024-04-14 21:06:15 -0400

    GAY

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,26 @@ struct Cli {
     res: u32,
     #[arg(short, long)]
     depth: usize,
-    #[arg(short, long)]
-    palette: String,
 }
 
-fn lerp(a: u32, b: u32, p: f64) -> PremultipliedColorU8 {
+fn make_color(c: u32) -> PremultipliedColorU8 {
+    let [r, g, b, a] = c.to_be_bytes();
+    ColorU8::from_rgba(r, g, b, a).premultiply()
+}
+
+fn lerp(a: u32, b: u32, p: f64) -> u32 {
     let [ar, ag, ab, aa] = a.to_be_bytes();
     let [br, bg, bb, ba] = b.to_be_bytes();
-    tiny_skia::ColorU8::from_rgba(
+    u32::from_be_bytes([
         (br as f64 * p + ar as f64 * (1.0 - p)).floor() as u8,
         (bg as f64 * p + ag as f64 * (1.0 - p)).floor() as u8,
         (bb as f64 * p + ab as f64 * (1.0 - p)).floor() as u8,
         (ba as f64 * p + aa as f64 * (1.0 - p)).floor() as u8,
-    ).premultiply()
+    ])
 }
 
-fn make_palette(colors: &[u32], size: usize) -> Vec<PremultipliedColorU8> {
-    let mut out = vec![PremultipliedColorU8::from_rgba(0, 0, 0, 0).unwrap(); size];
+fn make_palette(colors: &[u32], size: usize) -> Vec<u32> {
+    let mut out = vec![0; size];
     let lcm = num::integer::lcm(colors.len() - 1, size);
     let cs = lcm / (colors.len() - 1);
     let os = lcm / size;
@@ -36,8 +39,7 @@ fn make_palette(colors: &[u32], size: usize) -> Vec<PremultipliedColorU8> {
         let exp = i * os;
         let (div, rem) = num::integer::div_rem(exp, cs);
         if rem == 0 || div == colors.len() {
-            let [r, g, b, a] = colors[div].to_be_bytes();
-            *c = ColorU8::from_rgba(r, g, b, a).premultiply();
+            *c = colors[div];
         } else {
             *c = lerp(colors[div], colors[div + 1], (rem as f64) / (cs as f64));
         }
@@ -55,37 +57,21 @@ fn julia_depth(mut z: Complex64, c: Complex64, depth: usize) -> usize {
     depth
 }
 
-fn parse_palette(mut input: &str) -> Vec<u32> {
-    if input.starts_with('@') {
-        parse_palette(&std::fs::read_to_string(&input[1..]).unwrap())
-    } else {
-        let mut out = Vec::with_capacity(input.len() / 8);
-        loop {
-            input = input.trim_start();
-            let len = input.bytes().take(9).position(|i| !i.is_ascii_hexdigit()).unwrap_or(std::cmp::min(input.len(), 9));
-            let c = match len {
-                0 => break,
-                6 => (u32::from_str_radix(&input[..6], 16).unwrap() << 8) | 0xFF,
-                8 => u32::from_str_radix(&input[..8], 16).unwrap(),
-                l => panic!("expected 6 or 8 character input, found {l}!"),
-            };
-            input = &input[len..];
-            out.push(c);
-        }
-        out
-    }
-}
-
 fn main() {
+    const UPPER_PALETTE: &[u32] = &[0x078d70ff, 0x26ceaaff, 0x98e8c1ff, 0xffffffff];
+    const LOWER_PALETTE: &[u32] = &[0x3d1a38ff, 0x5049ccff, 0x7bade2ff, 0xffffffff];
     let cli = Cli::parse();
     let mut canvas = tiny_skia::Pixmap::new(cli.res, cli.res).unwrap();
-    let palette = make_palette(&parse_palette(&cli.palette), cli.depth + 1);
+    let upper = make_palette(UPPER_PALETTE, cli.depth + 1);
+    let lower = make_palette(LOWER_PALETTE, cli.depth + 1);
     let scale = 4.0 / (cli.res as f64);
     for (i, px) in canvas.pixels_mut().iter_mut().enumerate() {
         let (y, x) = num::integer::div_rem(i, cli.res as usize);
-        let z = Complex64::new(x as f64 * scale - 2.0, y as f64 * scale - 2.0);
+        let x = x as f64 * scale - 2.0;
+        let y = y as f64 * scale - 2.0;
+        let z = Complex64::new(x, y);
         let depth = julia_depth(z, cli.c, cli.depth);
-        *px = palette[depth];
+        *px = make_color(lerp(upper[depth], lower[depth], y / 4.0 + 0.5));
     }
     canvas.save_png(cli.output).unwrap();
 }
